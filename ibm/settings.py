c7d976a6f6673ecd8865208290edad1f036c37e2
Haseeb Tariq <mhaseebtariq@gmail.com>
2025-08-01 21:11:20 +0200

    Reported

--- ibm/settings.py
+++ ibm/settings.py
@@ -4,8 +4,8 @@ import os
 TIMESTAMP_FORMAT = "yyyy/MM/dd HH:mm"
 VALID_SIZES = ["Small", "Medium", "Large"]
 
-HIGH_ILLICIT = False
-FILE_SIZE = "Small"
+HIGH_ILLICIT = True
+FILE_SIZE = "Large"
 
 assert FILE_SIZE in VALID_SIZES
 ILLICIT_TYPE = "HI" if HIGH_ILLICIT else "LI"
